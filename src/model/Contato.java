ccf611211193e1156b9111c87662fb49d3db77ba
Anthony Louis <anthony.physis@gmail.com>
2018-06-22 19:03:36 -0300

    Criando javadoc

--- src/model/Contato.java
+++ src/model/Contato.java
@@ -27,106 +27,184 @@ public class Contato {
         listaRedeSocial = new ArrayList<>();
     }
 
+    /**
+     * @return
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * @param nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * @return
+     */
     public String getNomeFonetico() {
         return nomeFonetico;
     }
 
+    /**
+     * @param nomeFonetico
+     */
     public void setNomeFonetico(String nomeFonetico) {
         this.nomeFonetico = nomeFonetico;
     }
 
+    /**
+     * @return
+     */
     public String getSobrenome() {
         return sobrenome;
     }
 
+    /**
+     * @param sobrenome
+     */
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
 
+    /**
+     * @return
+     */
     public String getSobrenomeFonetico() {
         return sobrenomeFonetico;
     }
 
+    /**
+     * @param sobrenomeFonetico
+     */
     public void setSobrenomeFonetico(String sobrenomeFonetico) {
         this.sobrenomeFonetico = sobrenomeFonetico;
     }
 
+    /**
+     * @return
+     */
     public String getEmpresa() {
         return empresa;
     }
 
+    /**
+     * @param empresa
+     */
     public void setEmpresa(String empresa) {
         this.empresa = empresa;
     }
 
+    /**
+     * @return
+     */
     public String getEmpresaFonetico() {
         return empresaFonetico;
     }
 
+    /**
+     * @param empresaFonetico
+     */
     public void setEmpresaFonetico(String empresaFonetico) {
         this.empresaFonetico = empresaFonetico;
     }
 
+    /**
+     * @return
+     */
     public ArrayList<String> getListaTelefone() {
         return listaTelefone;
     }
 
+    /**
+     * @param listaTelefone
+     */
     public void setListaTelefone(ArrayList<String> listaTelefone) {
         this.listaTelefone = listaTelefone;
     }
 
+    /**
+     * @return
+     */
     public ArrayList<String> getListaEmail() {
         return listaEmail;
     }
 
+    /**
+     * @param listaEmail
+     */
     public void setListaEmail(ArrayList<String> listaEmail) {
         this.listaEmail = listaEmail;
     }
 
+    /**
+     * @return
+     */
     public ArrayList<String> getListaURL() {
         return listaURL;
     }
 
+    /**
+     * @param listaURL
+     */
     public void setListaURL(ArrayList<String> listaURL) {
         this.listaURL = listaURL;
     }
 
+    /**
+     * @return
+     */
     public ArrayList<Endereco> getListaEndereco() {
         return listaEndereco;
     }
 
+    /**
+     * @param listaEndereco
+     */
     public void setListaEndereco(ArrayList<Endereco> listaEndereco) {
         this.listaEndereco = listaEndereco;
     }
 
+    /**
+     * @return
+     */
     public Date getDataAniversario() {
         return dataAniversario;
     }
 
+    /**
+     * @param dataAniversario
+     */
     public void setDataAniversario(Date dataAniversario) {
         this.dataAniversario = dataAniversario;
     }
 
+    /**
+     * @return
+     */
     public ArrayList<String> getListaRedeSocial() {
         return listaRedeSocial;
     }
 
+    /**
+     * @param listaRedeSocial
+     */
     public void setListaRedeSocial(ArrayList<String> listaRedeSocial) {
         this.listaRedeSocial = listaRedeSocial;
     }
 
+    /**
+     * @return
+     */
     public int getIdBanco() {
         return idBanco;
     }
 
+    /**
+     * @param idBanco
+     */
     public void setIdBanco(int idBanco) {
         this.idBanco = idBanco;
     }
--- src/model/Endereco.java
+++ src/model/Endereco.java
@@ -10,62 +10,107 @@ public class Endereco {
     private int CEP;
     private String pais;
 
+    /**
+     * @return
+     */
     public String getPais() {
         return pais;
     }
 
+    /**
+     * @param pais
+     */
     public void setPais(String pais) {
         this.pais = pais;
     }
 
+    /**
+     * @return
+     */
     public String getRua() {
         return rua;
     }
 
+    /**
+     * @param rua
+     */
     public void setRua(String rua) {
         this.rua = rua;
     }
 
+    /**
+     * @return
+     */
     public String getComplemento() {
         return complemento;
     }
 
+    /**
+     * @param complemento
+     */
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
 
+    /**
+     * @return
+     */
     public String getBairro() {
         return bairro;
     }
 
+    /**
+     * @param bairro
+     */
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
 
+    /**
+     * @return
+     */
     public String getCidade() {
         return cidade;
     }
 
+    /**
+     * @param cidade
+     */
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
 
+    /**
+     * @return
+     */
     public String getEstado() {
         return estado;
     }
 
+    /**
+     * @param estado
+     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
+    /**
+     * @return
+     */
     public int getCEP() {
         return CEP;
     }
 
+    /**
+     * @param CEP
+     */
     public void setCEP(int CEP) {
         this.CEP = CEP;
     }
 
+    /**
+     * @return
+     */
     @Override
     public String toString(){
         return rua + " " + complemento + " " + bairro + " " + cidade + " " + estado + " " + pais + "," + CEP;
